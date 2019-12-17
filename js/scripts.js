adcf7f4ddca1bd7f2499c3131a2204182e24c9cd
matt henley <mattbhenley@gmail.com>
2019-12-17 17:32:34 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-/*
- * This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License. 
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/.
- * Copyright (c) 2016 Julian Garnier
- */
-
 window.onload = function() {
 
   var messagesEl = document.querySelector('.messages');
